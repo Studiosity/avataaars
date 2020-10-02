3dac64de3821d94ad1846e0f7b74cad788c1d327
Daniel Teale <dteale@studiosity.com>
2020-10-02 16:05:32 +1000

    changed componentDidMount to UNSAFE_componentWillMount

--- src/options/Selector.tsx
+++ src/options/Selector.tsx
@@ -26,7 +26,7 @@ export default class Selector extends React.Component<Props> {
     return this.context.optionContext
   }
 
-  componentDidMount(): void {
+  UNSAFE_componentWillMount(): void {
     const { option, defaultOption } = this.props
     const { optionContext } = this
     const defaultValue = (
